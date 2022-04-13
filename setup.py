8892acfc7cde3a5cb43b7e3894ae92d8c1950711
Andrew Danks <andrew.danks@affirm.com>
2022-04-13 16:18:21 -0700

    bump version

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ if sys.version_info[:2] < (2, 7):
 
 setup(
     name='luigi1',
-    version='1.1.2+affirm.1.1.0',
+    version='1.1.2+affirm.1.1.1',
     description='Workflow mgmgt + task scheduling + dependency resolution',
     long_description=long_description,
     author='Erik Bernhardsson',
